33bb3b0741b4ab2a1935d0a90028a562f30cfdb0
ภัทรชัย บริบูรณ์ <pwppowerpack@gmail.com>
2022-03-27 22:11:54 +0700

    Add a description (2)

--- sudoku.py
+++ sudoku.py
@@ -12,7 +12,7 @@ class Sudoku():
 
     def _is_valid_grid(self):
         for i in range(9):
-            # Check row
+            # เช็คแถว
             possible_row = [True for v in range(9)]
             for j in range(9):
                 val = self.grid[i][j]
@@ -21,7 +21,7 @@ class Sudoku():
                         possible_row[val - 1] = False
                     else:
                         return False
-            # Check column
+            # เช็คคอลัมน์
             possible_col = [True for v in range(9)]
             for j in range(9):
                 val = self.grid[j][i]
@@ -30,7 +30,7 @@ class Sudoku():
                         possible_col[val - 1] = False
                     else:
                         return False
-            # Check square
+            # เช็คตารางย่อย
             possible_squ = [True for v in range(9)]
             row_min, col_min = 3 * (i // 3), 3 * (i  % 3)
             for j in range(row_min, row_min + 3):
@@ -44,42 +44,42 @@ class Sudoku():
         return True
 
     def _solve(self):
-        # If no free cell can be found, then the grid is already filled
+        # หากไม่พบช่องที่ว่าง แสดงว่าตารางเต็มแล้ว
         row, col, values = self._get_free_cell()
         if row < 0 or col < 0:
             return True
-        # Try all possible values for the given cell
+        # ลองค่าที่เป็นไปได้ทั้งหมดสำหรับช่องนั้น ๆ
         for v in values:
-            # Try to fill the grid with this value
+            # ลองใส่ค่านั้น ในตาราง
             self.grid[row][col] = v;
             if self._solve():
                 return True
-            # Impossible to find a solution with this value: revert the change
+            # ถ้าไม่มีค่าใดเป็นไปได้ ก็คืนค่าเดิม
             self.grid[row][col] = 0;
         return False
 
     def _get_free_cell(self):
         row, col, values = -1, -1, []
-        # Test all cells in the grid
+        # เทสช่องทั้งหมดในตาราง
         for i in range(9):
             for j in range(9):
-                # Check that the cell has not been assigned a value yet
+                # เช็คหาช่องที่ว่าง
                 if self.grid[i][j] != 0:
                     continue
-                # If it is more constrained than any other one, get the possible values for this cell
+                # หาค่าที่เป็นไปได้สำหรับช่องนั้น ๆ
                 ok, cell_values = self._get_possible_values(i, j, len(values) if len(values) > 0 else 10)
                 if ok:
                     row    = i
                     col    = j
                     values = cell_values
-                    if len(values) <= 1:  # It cannot be better than 1 possible value
+                    if len(values) <= 1:  # ถ้ามีแค่ค่าเดียว ก็ใส่ค่านั้นไป
                         return row, col, values
         return row, col, values
 
     def _get_possible_values(self, row, col, only_if_better_than):
         nb_possible = 9
         possible    = [True,True,True,True,True,True,True,True,True]
-        # Check along the row and column
+        # เช็คตามแถวและคอลัมน์
         for i in range(9):
             val_row = self.grid[row][i]
             if val_row > 0 and possible[val_row - 1]:
@@ -89,7 +89,7 @@ class Sudoku():
             if val_col > 0 and possible[val_col - 1]:
                 possible[val_col - 1] = False
                 nb_possible -= 1
-        # Check in the square
+        # เช็คในตารางย่อย
         row_min, col_min = 3 * (row // 3), 3 * (col // 3);
         for i in range(row_min, row_min + 3):
             for j in range(col_min, col_min + 3):
@@ -97,7 +97,7 @@ class Sudoku():
                 if val > 0 and possible[val - 1]:
                     possible[val - 1] = False
                     nb_possible -= 1
-        # Output possible values
+        # ใส่ค่าที่เป็นไปได้ทั้งหมด
         if nb_possible < only_if_better_than:
             values = []
             for i in range(9):
