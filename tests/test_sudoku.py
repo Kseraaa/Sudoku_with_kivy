1c9a716688e4e526fa22456c9f29d6772c47943d
ปฏิญญา ลิ่มเฮ่า <teoylimhao2@gmail.com>
2022-03-30 22:30:01 +0700

    fixed alltestcase

--- tests/test_sudoku.py
+++ tests/test_sudoku.py
@@ -5,24 +5,24 @@ class Sudoku_Test(unittest.TestCase):
 
     def test_sudoku_zaza1(self):
         sudo = Sudoku([[0 for col in range(9)] for row in range(9)]) #set0ทั้งตารางไม่มีช่องว่าง
-        res =sudo.get_value(5, 1) #เอาrow5col1
+        res = sudo.get_value(5, 1) #เอาrow5col1
         res = sudo.solve()
         self.assertTrue(res)
 
     def test_sudoku_zaza2(self):
         sudo = Sudoku([[0 for col in range(9)] for row in range(9)]) #set0ทั้งตารางไม่มีช่องว่าง
-        res =sudo.get_value(6, 2) #เอาrow6col2
+        res = sudo.get_value(6, 2) #เอาrow6col2
         res = sudo.solve()
         self.assertTrue(res)
     
     def test_sudoku_zaza3(self):
         sudo = Sudoku([[0 for col in range(9)] for row in range(9)]) #set0ทั้งตารางไม่มีช่องว่าง
-        res =sudo.get_value(3, 3) #เอาrow3col3
+        res = sudo.get_value(3, 3) #เอาrow3col3
         res = sudo.solve()
         self.assertTrue(res)
 
     def test_sudoku_zaza4(self):
         sudo = Sudoku([[0 for col in range(9)] for row in range(9)]) #set0ทั้งตารางไม่มีช่องว่าง
-        res =sudo.get_value(8, 2) #เอาrow8col2
+        res = sudo.get_value(8, 2) #เอาrow8col2
         res = sudo.solve()
         self.assertTrue(res)
